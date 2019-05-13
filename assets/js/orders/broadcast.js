002189e9d9183bf5b8d8e6ea03b71c9bc0165288
Prncssbby <guico.princess@gmail.com>
2019-05-14 06:52:21 +0800

    set ajax broadcast to async false

--- assets/js/orders/broadcast.js
+++ assets/js/orders/broadcast.js
@@ -3,6 +3,30 @@ var base_url = $('body').data('urlbase');
 var image_url = "https://pick-n-ride.000webhostapp.com/";
 var access_token = "EAADuLIY8BksBALxFZA9SuJ5vshC6UB1mBmbwnjie1fkf4sXXeVfmiUZAuJoZAbM5NYmyo6PMByeuUibI7vK5TAib5uGC2hzj4BfbXZAFEUlwNSZA5TcbzcnNCWTkVEqkuZAx26MkirY85dext2casBL0s4x5R9Mj9ttxpsQe1IxwZDZD";
 
+
+function sendMessage(recipient,message) {
+
+	var data = {
+		"recipient":{
+			"id":recipient
+		},
+		"message":{
+			"text":message
+		}
+	};
+
+	$.ajax({
+		url:"https://graph.facebook.com/v2.6/me/messages?access_token="+access_token,
+		type:"POST",
+		async:false,
+		contentType: "application/json",
+		data:data,
+		success:function(result){
+			console.log(result);
+		}
+	});
+}
+
 $('#broadcastBtn').click(function(){
 
 	var fbId = [];
@@ -11,26 +35,25 @@ $('#broadcastBtn').click(function(){
 	var rxFile = $('#rxFile').val();
 
 
-	$.each($("input[name='fbId']:checked"),function(){
+	$.each($("input[name='fbId']:checked"),function() {
 		fbId.push($(this).val());
-		fbImage.push($(this).val());
 	});
 
 
-	for(i =0; i < fbImage.length; i++) {
+	for(i =0; i < fbId.length; i++) {
 		
 
 		if(rxFile != "") {
 
 			var imageData = {
 				"recipient":{
-					"id":fbImage[i]
+					"id":fbId[i]
 				},
 				"message":{
 					"attachment":{
 						"type":"image",
 						"payload":{
-							"url":base_url+'/uploads/rx/'+rxFile,
+							"url":image_url+'/rx/'+rxFile,
 							"is_reusable":true
 						}
 					}
@@ -44,34 +67,11 @@ $('#broadcastBtn').click(function(){
 				contentType: "application/json",
 				data:imageData,
 				success:function(result) {
-
- 					for(i =0; i < fbId.length; i++) {
-						var data = {
-							"recipient":{
-								"id":fbId[i]
-							},
-							"message":{
-								"text":message
-							}
-						};
-
-						$.ajax({
-							url:"https://graph.facebook.com/v2.6/me/messages?access_token="+access_token,
-							type:"POST",
-							async:false,
-							contentType: "application/json",
-							data:data,
-							success:function(result){
-								console.log(result);
-							}
-						});
-					}
+					sendMessage(fbId[i],message);
 				}
-				
 			});
 
-
-
+		
 		}else {
 
 
