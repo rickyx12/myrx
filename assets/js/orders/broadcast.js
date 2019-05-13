5317fb9a1c677369d6e0253ac673b34f04f335a4
Prncssbby <guico.princess@gmail.com>
2019-05-14 05:51:55 +0800

    fix path of image in broadcast

--- assets/js/orders/broadcast.js
+++ assets/js/orders/broadcast.js
@@ -30,7 +30,7 @@ $('#broadcastBtn').click(function(){
 					"attachment":{
 						"type":"image",
 						"payload":{
-							"url":base_url+'/rx/'+rxFile,
+							"url":base_url+'/uploads/rx/'+rxFile,
 							"is_reusable":true
 						}
 					}
