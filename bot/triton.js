4bdfd0af74e6f80cf0d797e4afea83206a5f1ffd
Prncssbby <guico.princess@gmail.com>
2019-06-30 07:59:59 +0800

    debug

--- bot/triton.js
+++ bot/triton.js
@@ -37,6 +37,8 @@ app.post('/triton/webhook', (req, res) => {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
+      console.log(entry);
+
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
