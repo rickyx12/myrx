052a9f6eba1b44fc17800dce4596e6843b7496ab
Prncssbby <guico.princess@gmail.com>
2019-06-30 08:18:47 +0800

    debug

--- bot/triton.js
+++ bot/triton.js
@@ -137,7 +137,7 @@ function handleMessage(sender_psid, received_message) {
         // }
 
         sendAttachments(sender_psid);
-        console.log(received_message.attachments);
+        console.log(received_message.attachments.payload);
       }else {
 
         console.log('handle message fallback');
